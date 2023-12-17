5194e373b329f26788fdf9c0b286a51df2b74c9b
Giorgos Stamelakis <georgios.stamelakis@atos.net>
2023-12-17 18:24:05 +0200

    Initial Commit

---
+++ MyPokemon/MyPokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPokemonApp.swift
+//  MyPokemon
+//
+//  Created by Georgios Stamelakis on 17/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MyPokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
