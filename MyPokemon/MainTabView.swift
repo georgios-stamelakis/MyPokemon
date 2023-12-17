f62e3ca5074d072ca2b75c5333ee45f25634be6d
Giorgos Stamelakis <georgios.stamelakis@atos.net>
2023-12-17 19:59:18 +0200

    Added top bar for location and profile

--- MyPokemon/MainTabView.swift
+++ MyPokemon/MainTabView.swift
@@ -8,30 +8,83 @@
 import SwiftUI
 
 struct MainTabView: View {
+    @State private var isLocationPresented = false
+    @State private var isProfilePresented = false
+
     var body: some View {
-        TabView {
-            // Tab 1
-            HomeView()
-                .tabItem {
-                    Image(systemName: "house")
-                    Text("HOME")
+        VStack {
+
+            // Top Bar
+            HStack {
+                // Location Button
+                Button(action: {
+                    isLocationPresented.toggle()
+                }) {
+                    Image(systemName: "location.fill")
+                        .padding()
+                }
+                .sheet(isPresented: $isLocationPresented) {
+                    LocationView()
                 }
 
-            // Tab 2
-            Text("Search is Under Construction!")
-                .tabItem {
-                    Image(systemName: "magnifyingglass")
-                    Text("SEARCH")
-                }
+                Spacer()
 
-            // Tab 3
-            Text("Tickets is Under Construction")
-                .tabItem {
-                    Image(systemName: "ticket")
-                    Text("TICKETS")
+                // Sign In/Profile Button
+                Button(action: {
+                    isProfilePresented.toggle()
+                }) {
+                    Image(systemName: "person.fill")
+                        .padding()
                 }
+                .sheet(isPresented: $isProfilePresented) {
+                    ProfileView()
+                }
+            }
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
 
+            // Tab View
+            TabView {
+                // Tab 1
+                HomeView()
+                    .tabItem {
+                        Image(systemName: "house")
+                        Text("HOME")
+                    }
+
+                // Tab 2
+                Text("Search is Under Construction!")
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                        Text("SEARCH")
+                    }
+
+                // Tab 3
+                Text("Tickets is Under Construction")
+                    .tabItem {
+                        Image(systemName: "ticket")
+                        Text("TICKETS")
+                    }
+
+            }
         }
+    }
+}
+
+struct LocationView: View {
+    var body: some View {
+        // Replace with your location view content
+        Text("Location View")
+            .foregroundColor(.black)
+    }
+}
+
+struct ProfileView: View {
+    var body: some View {
+        // Replace with your profile view content
+        Text("Profile View")
+            .foregroundColor(.black)
     }
 }
 
